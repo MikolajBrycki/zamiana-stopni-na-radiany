f46cf9439f062986226a04b60eb45c143be01599
MikolajBrycki <36439202+MikolajBrycki@users.noreply.github.com>
2018-02-16 10:09:39 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+double stopnie;
+cout << "Podaj kat w stopniach: ";
+cin >> stopnie;
+double radiany =( stopnie * M_PI ) / 180.0f;
+cout<< ""<<endl;
+cout<<stopnie<<" stopni w radiantach wynosi: "<<radiany<<endl;
+    return 0;
+}
+
